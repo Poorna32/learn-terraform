18bfa495db3cea138b683f73167b92d83021572d
Poorna32 <poornachandraga44@gmail.com>
2024-05-01 16:29:36 +0530

    module demo

--- 05-module/demo/ec2.tf
+++ 05-module/demo/ec2.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "test" {
   ami           = "ami-090252cbe067a9e58"
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
 
   tags = {
     name = var.name
   }
 }
 variable "name" {}
+variable "instance_type" {}
