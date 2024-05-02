50be3e79754e0ea6fd12f76e1ac0b920065caf4b
Poorna32 <poornachandraga44@gmail.com>
2024-05-02 14:14:03 +0530

    remote state

--- 09-multi-state/main.tf
+++ 09-multi-state/main.tf
@@ -1,9 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket = "tf-states3"
-    key    = "test1/${var.env}/state"
-    region = "us-east-1"
-  }
+  backend "s3" {}
 }
 
 output "env" {
---
+++ 09-multi-state/prod-backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "tf-states3"
+key    = "test1/prod/state"
+region = "us-east-1"
