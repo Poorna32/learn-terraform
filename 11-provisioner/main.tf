2c44718c24e7d33cb438ac1f16b1d19d107ba507
Poorna32 <poornachandraga44@gmail.com>
2024-05-03 12:41:15 +0530

    provisioner demo

--- 11-provisioner/main.tf
+++ 11-provisioner/main.tf
@@ -23,3 +23,35 @@ resource "aws_instance" "test" {
 data "aws_security_group" "selected" {
    name = "allow-all"
 }
+
+#decouple provisioner in following scenarios
+
+#1.if provisioner is fails then instance we dont want to re create my instance
+#2.requirement emergence in a such a way that we need to re run the provisioner again an again
+
+resource "aws_instance" "test1" {
+  ami           = "ami-090252cbe067a9e58"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
+}
+
+resource "null_resource" "provisioner" {
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.test1.public_ip
+    }
+
+    inline = [
+      "install nginx -y",
+      "sudo systemctl start nginx"
+    ]
+  }
+
+}
+
+
+}
